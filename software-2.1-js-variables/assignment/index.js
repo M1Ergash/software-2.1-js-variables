184e7de58022dc4acd30d27abfb5bef5074b8fe1
Ergash <masharipov.console@gmail.com>
2023-05-08 20:06:55 +0500

    homework

--- software-2.1-js-variables/assignment/index.js
+++ software-2.1-js-variables/assignment/index.js
@@ -1,26 +1,20 @@
-let studentFirstName = "Brandon";
-let studentLastName = "N";
-let grade1 = 53;
-let grade2 = 96;
-let grade3 = 65;
-let grade4 = 74;
-let passingGrade = 80;
-``;
-//1.Create a variable called average that computes for the average from grade1 to grade4
-let average = (grade1 + grade2 + grade3 + grade4) / 4;
+//Write JS code that solve the following problems
+//Q1. Using an if-else statement, determine if a number is divisible by 3, by 5, or both.
 
-//2. Change the value of studentLastName to "Newman"
-studentLastName = "Newman";
+let q1test = 15;
 
-//3. Print in console the following statement: "<value of studentFirstName> <value of studentLastName> has an average grade of <value of average>"
-console.log(
-  `${studentFirstName} ${studentLastName} has an average grade of ${average}`
-);
-//ex. Jane Doe has an average grade of 100.
+//Add code here
 
-//4. Write code that would answer this question: Did the student pass?
-function isStudentPassed(average) {
-  return average >= 75;
-}
+//Q2. Using an if-else statement, determin is a year is a leap year or not.
+//Research what are the conditions to be considered a leap year.
 
-isStudentPassed(average);
+let year = 1999;
+// let year = 2000;
+// let year = 1900;
+// let year = 2024;
+
+//Add code here
+
+//Q3. Use a loop to print all the numbers from 1 to 100 that are divisible by 3 or 4.
+
+//Add code here
